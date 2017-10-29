56f1af409957a1c807257103b4ce0db498a18eee
Erkan <Erkan@ORION>
2017-10-29 23:50:09 +0300

    v1.1

--- TdoaSimulator/TdoaSimulator/Form1.Designer.cs
+++ TdoaSimulator/TdoaSimulator/Form1.Designer.cs
@@ -33,7 +33,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(553, 350);
+			this.ClientSize = new System.Drawing.Size(582, 369);
 			this.Name = "Form1";
 			this.Text = "Tdoa Simulator";
 			this.ResumeLayout(false);
